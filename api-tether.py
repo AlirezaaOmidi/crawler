8ae8c302a133f2d4a61199db29ed19d944d977ea
AlirezaaOmidi <alireza.om97@gmail.com>
2025-06-15 14:42:44 +0330

    nwe

--- api-tether.py
+++ api-tether.py
@@ -840,7 +840,7 @@ def getting_data():
         url_wallex = "https://api.wallex.ir/v1/trades?symbol=USDTTMN"
         url_tabdeal = "https://api-web.tabdeal.org/r/plots/currency_prices/"
         url_ramzinex = "https://publicapi.ramzinex.com/exchange/api/v1.0/exchange/chart/statistics-24/11"
-        url_exir = "https://api.exir.io/v2/ticker?symbol=usdt-irt11111111111111111111"
+        url_exir = "11111111111111111111https://api.exir.io/v2/ticker?symbol=usdt-irt"
         url_tetherland = "https://api.tetherland.com/currencies1111111111"
         url_ok_ex = "https://azapi.ok-ex.io/oapi/v1/market/tickers111111111111"
         url_aban = 'https://abantether.com/coin/USDT'
